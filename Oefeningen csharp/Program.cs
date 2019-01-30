a975ff881270915c9b3ab0d8afe5304ec6d340b1
Twan Natter <twan180401@gmail.com>
2019-01-30 16:01:48 +0100

    Opdracht 2-1

--- Oefeningen csharp/Program.cs
+++ Oefeningen csharp/Program.cs
@@ -6,8 +6,30 @@ namespace Oefeningen_csharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hallo Wereld!");
-            Console.WriteLine("Druk op ENTER om de console af te sluiten…");
+            //Kleuren instellen
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+
+            //Declaratie van de variabelen
+            int intGetal1, intGetal2, intSom;
+
+            //De 2 getallen opvragen
+            Console.Write("Geef getal1: ");
+            intGetal1 = Convert.ToInt16(Console.ReadLine());
+
+            Console.Write("Geef getal2: ");
+            intGetal2 = Convert.ToInt16(Console.ReadLine());
+
+            //De som berekenen
+            intSom = intGetal1 + intGetal2;
+
+            //Kleuren instellen
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            //Het resultaat weergeven in de console
+            Console.WriteLine("De som bedraagt " + intSom.ToString());
             Console.ReadLine();
         }
     }
