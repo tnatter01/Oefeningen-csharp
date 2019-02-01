1cca5f874e976b69d81b17aaf2a36650d196897d
Twan Natter <twan180401@gmail.com>
2019-02-01 13:41:06 +0100

    Opdracht 3.1

--- Oefeningen csharp/Program.cs
+++ Oefeningen csharp/Program.cs
@@ -7,17 +7,21 @@ namespace Oefeningen_csharp
         static void Main(string[] args)
         {
             //Declaratie van de variabelen
-            double dblEclBTW, dblInclBTW;
+            double dblHoekA, dblHoekC, dblAfstandAB;
+            const double cdblAfstandAC = 10;
 
-            //Bedrag in euro's opvragen
-            Console.Write("Geef bedrag in euro's: ");
-            dblEclBTW = Convert.ToInt16(Console.ReadLine());
+            //Opvragen hoeken A en C
+            Console.Write("De grootte van hoek A (in graden) =  ");
+            dblHoekA = Convert.ToDouble(Console.ReadLine());
+            Console.Write("De grootte van hoek C (in graden) =  ");
+            dblHoekC = Convert.ToDouble(Console.ReadLine());
 
-            //De som berekenen
-            dblInclBTW = dblEclBTW * 1.21;
+            //Afstand AB Berkenen
+            dblAfstandAB = (cdblAfstandAC * Math.Sin(dblHoekC * Math.PI / 180)) / (Math.Sin((dblHoekA * Math.PI / 180) + (dblHoekC * Math.PI / 180)));
 
             //Het resultaat weergeven in de console
-            Console.WriteLine("De prijs incl. BTW bedraagt " + dblInclBTW.ToString() + ".");
+            Console.WriteLine();
+            Console.WriteLine("De afstand AB = " + Math.Round(dblAfstandAB,1).ToString() + " meter.");
             Console.ReadLine();
         }
     }
